0c7fef569c01b863161eb9045e6e4c754cd19652
Josip Sime <josipsime@hotmail.com>
2020-05-07 19:41:31 +0200

    player/base

--- SpaceRts/Structures/Base.cs
+++ SpaceRts/Structures/Base.cs
@@ -1,10 +1,41 @@
-﻿using System;
+﻿using SpaceRts.Map;
+using System;
 namespace SpaceRts.Structures
 {
     public class Base
     {
-        public Base()
+        public int x;
+        public int y;
+        private int cost;
+        private int prod;
+        public Cell cell;
+        public Player player;
+        //private model 
+        //wpublic int [] ;
+        public Base(int x, int y, Cell cell, int cost, int prod, Player player)
         {
+            
+            this.x = x;
+            this.y = y;
+            this.cell = cell;
+            this.cost = cost;
+            this.prod = prod;
+            this.player = player;
+         }
+
+        public void Build()
+        {
+            player.pay(cost);
+            //cell.draw(model);
         }
+
+        public int Production()
+        {
+            return this.prod;
+        }
+
+
+             
+        
     }
 }
